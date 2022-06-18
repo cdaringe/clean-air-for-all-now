d5d521f0d64926b89779eca5dd71aac2af990dad
cdaringe <cdaringe@gmail.com>
2022-06-18 15:06:19 -0700

    fix: image rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import { Mission } from "../src/components/mission";
 import Featured from "../src/components/featured";
 import { Link } from "../src/components/link";
+import Image from "next/image";
 
 export default function Home(props) {
   return (
@@ -22,10 +22,13 @@ export default function Home(props) {
             Donate Now!
           </a>
           <Featured />
-          <img
+          <Image
             alt="intel rancho"
-            src="intel-rancho.jpg"
+            src="/intel-rancho.jpg"
             className="shadow-lg rounded"
+            layout="responsive"
+            width={4032}
+            height={3024}
           />
           <p className="text-gray">
             The Intel plant in Rio Rancho spews toxic fumes above a residential
@@ -34,10 +37,13 @@ export default function Home(props) {
             buyers.
           </p>
           <Link href="/intel-rancho-map.jpeg">
-            <img
+            <Image
               alt="intel rancho map"
-              src="intel-rancho-map.jpeg"
+              src="/intel-rancho-map.jpeg"
               className="shadow-lg rounded mt-2 mb-2 block"
+              layout="responsive"
+              width={1570}
+              height={944}
             />
           </Link>
           <span className="text-gray-800 text-sm w-full italic">
